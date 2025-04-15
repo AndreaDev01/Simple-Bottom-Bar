a56303ede606044b8bf66645550b751ada3d456f
Andreaaas <andreadevelopersalvatore@gmail.com>
2025-04-15 21:20:34 +0200

    Create example app to show how plugin work

---
+++ example/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:simple_bottom_bar/simple_bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Bottom bar example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Simple bottom bar example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<MenuItem> widgets =[
+    MenuItem(index: 0, text: "Home Page", icon: Icons.home, widget: Container(child: Text("Home page"),)),
+    MenuItem(index: 0, text: "Account", icon: Icons.people, widget: Container(child: Text("Account"),)),
+    MenuItem(index: 0, text: "Settings", icon: Icons.settings, widget: Container(child: Text("Settings"),)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return AppContainer(
+        widgets: widgets,
+        backgroundColor: Colors.green.withOpacity(0.5)
+    );
+  }
+}
