258576bb4b4e5b2d1d51fb9f33154327555ad658
Andreaaas <andreadevelopersalvatore@gmail.com>
2025-04-16 09:28:06 +0200

    Fix best practices and adding documentation for the objects

--- lib/src/menu_item.dart
+++ lib/src/menu_item.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
+/// Menu item of bottom bar
 class MenuItem{
+  /// index of item
   final int index;
+  /// text disoplay on the item
   final String text;
+
+  /// icon display on the item
   final IconData icon;
+
+  /// The widget that is open when the index was selected
   final Widget widget;
 
+  /// Menu item of appbar, is required the fields index, text, icon and widget
   MenuItem({required this.index,required this.text,required this.icon, required this.widget});
 }
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/cupertino.dart';
-double height_appbar = 120;
-double heigth_bottom_bar = 80;
 
+/// The heigth container of bottom bar
+double heigthBottomBar = 80;
+/// return the Heigth of screen
 getHeigth(BuildContext context){
   return MediaQuery.of(context).size.height;
 }
-
+/// return the width of Screen
 getWidth(BuildContext context){
   return MediaQuery.of(context).size.width;
-}
-
-getHeightContainer(BuildContext context){
-  return (getHeigth(context) - height_appbar - heigth_bottom_bar) - 20;
 }
